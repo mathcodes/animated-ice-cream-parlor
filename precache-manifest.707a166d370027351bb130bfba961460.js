1e4db1abe035f286efa443bdf3a1167638ff3f66
Jon Christie <jonpchristie@gmail.com>
2022-01-26 07:58:38 -0500

    Updates

---
+++ precache-manifest.707a166d370027351bb130bfba961460.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb89a76ccdf51b1cfddd0833e4654116",
+    "url": "/animated-ice-cream-parlor/index.html"
+  },
+  {
+    "revision": "34704d0079ed030c8cf8",
+    "url": "/animated-ice-cream-parlor/static/css/main.fa43fe42.chunk.css"
+  },
+  {
+    "revision": "c06c8c71c1e66288b81d",
+    "url": "/animated-ice-cream-parlor/static/js/2.ba54722c.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/animated-ice-cream-parlor/static/js/2.ba54722c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34704d0079ed030c8cf8",
+    "url": "/animated-ice-cream-parlor/static/js/main.2a80f99d.chunk.js"
+  },
+  {
+    "revision": "c91726d7b66087fcffac",
+    "url": "/animated-ice-cream-parlor/static/js/runtime-main.c82f634b.js"
+  }
+]);
